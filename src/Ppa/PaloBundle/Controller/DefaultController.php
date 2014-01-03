06af4bc45cc1ab6f7478e27babf31198166cbb18
Diego Navarro <diegonavarro42@gmail.com>
2014-01-03 14:59:27 -0300

    aparecen todos los productos en el index

--- src/Ppa/PaloBundle/Controller/DefaultController.php
+++ src/Ppa/PaloBundle/Controller/DefaultController.php
@@ -13,14 +13,24 @@ use Ppa\PaloBundle\Form\ProductoType;
 
 class DefaultController extends Controller
 {
-	
     public function indexAction()
     {
         $em = $this->getDoctrine()->getManager();
 
-        $entities = $em->getRepository('PpaPaloBundle:Producto')->findAll();
+        $entitie = $em->getRepository('PpaPaloBundle:Producto')->findAll();
 
         return $this->render('PpaPaloBundle:Default:index.html.twig', array(
+            'entitie' => $entitie,
+        ));
+    }
+
+    public function listaAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('PpaPaloBundle:TipoProducto')->findAll();
+
+        return $this->render('PpaPaloBundle:Default:lista.html.twig', array(
             'entities' => $entities,
         ));
     }
